6b7ff29c6d9d7507dddcb2dd686feae2d70f2122
Shuyib <benmainye@gmail.com>
2022-10-22 11:06:40 +0300

    Format code.

--- key-concepts-RL-agent.py
+++ key-concepts-RL-agent.py
@@ -1,15 +1,7 @@
-# Create an agent which can get through a maze
-# It should have appropriate functions for learning,choosing actions,and updating its memory.
+"""
+A module to handle an RL agent that finds what's the exit of the maze using numpy and the standard python library.
+"""
 
-# Create a class named agent add a function called learn, memory and actions
-# hasty first attempt
-# class Agent:
-#     def __init__(self, name, action, stateHistory):
-#         self.name = name
-#         self.action = action
-#         self.stateHistory = {action}
-
-# formatted with black
 import numpy as np
 
 
@@ -41,7 +33,7 @@ class Agent(object):
         ----------
         states : what has changed that is, the robot position. It ranges
         between -0.1 and -1.0.
-            
+
 
         Returns
         -------
@@ -73,47 +65,6 @@ class Agent(object):
         self.stateHistory = []
 
 
-# 6*6 maze, Update moves, check if game over, get the state, get rewards, print maze
-# robot position 2
-# class Environment:
-#     def __init__(self, state):
-#         self.state = None
-
-#     def createMaze(self, noofrows=6, noofcolumns=6):
-#         maze = np.zeros(shape=(noofrows, noofcolumns))
-#         global maze
-
-#     def printMaze(self):
-#         return maze
-
-#     def updateMove(self, rowindice, columnindice, maxmoves=[0]):
-#         position = np.put(maze, [rowindice, columnindice], 1)
-#         maxmoves[0] += 1
-#         global maxmoves
-#         global rowindice, columnindice
-#         return position
-
-#     def gameOver(self):
-#         if maxmoves >= 10:
-#             print("Game Over")
-#         else:
-#             print("Still have")
-#         return maxmoves
-
-#     def getReward(self):  # can't think of a way to update
-#         if position != 1 | maxmoves < 10:
-#             continue
-#         else:
-#             StopIteration()
-
-#     def getState(self):
-#         current_state = []
-#         return current_state.append(rowindice, columnindice)
-
-#     def getState(self):
-#         return self.state
-
-# some similar components to the one above issue contextuatizing the maze and getting ahead of myself.
 class Maze:
     """An RL environment for a robot in a 6 row and 6 column matrix trying to find its way to the objective.
     obstacles are labelled 1, robot position identity is 2 at the position 0,0 in a tuple.
@@ -145,14 +96,14 @@ class Maze:
         Parameters
         ----------
         state :
-            
+
         action :
-            
+
 
         Returns
         -------
 
-        
+
         """
         pass
 
@@ -162,12 +113,12 @@ class Maze:
         Parameters
         ----------
         action :
-            
+
 
         Returns
         -------
 
-        
+
         """
         pass
 
@@ -185,11 +136,11 @@ class Maze:
         Parameters
         ----------
         state :
-            
+
 
         Returns
         -------
 
-        
+
         """
         pass
